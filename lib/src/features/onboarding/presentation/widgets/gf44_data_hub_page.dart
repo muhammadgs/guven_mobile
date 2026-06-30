41f8d432cf6cec5afcfe5c6415e01a997d37e79a
muhammadgs <mehemmedg2006@gmail.com>
2026-06-30 17:28:59 +0400

    Add GF44 data hub page shell

---
+++ lib/src/features/onboarding/presentation/widgets/gf44_data_hub_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Gf44DataHubPage extends StatelessWidget {
+  const Gf44DataHubPage({
+    super.key,
+    required this.pageController,
+    required this.pageIndex,
+  });
+
+  final PageController pageController;
+  final int pageIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
